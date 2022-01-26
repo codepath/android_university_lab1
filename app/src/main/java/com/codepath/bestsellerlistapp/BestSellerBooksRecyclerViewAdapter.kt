8fbf55bf73211475e101976a7f80aea8155d0fd1
EJamdar05 <jamdareshaq@gmail.com>
2022-01-26 11:22:00 -0800

    Added api elements

--- app/src/main/java/com/codepath/bestsellerlistapp/BestSellerBooksRecyclerViewAdapter.kt
+++ app/src/main/java/com/codepath/bestsellerlistapp/BestSellerBooksRecyclerViewAdapter.kt
@@ -1,8 +1,12 @@
 package com.codepath.bestsellerlistapp
 
+import android.graphics.drawable.Drawable
+import android.media.Image
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import androidx.recyclerview.widget.RecyclerView.Adapter
@@ -11,6 +15,12 @@ import com.codepath.bestsellerlistapp.BestSellerBooksRecyclerViewAdapter.BookVie
 import com.codepath.bestsellerlistapp.R.id
 import com.codepath.bestsellerlistapp.R.layout
 import com.codepath.bestsellerlistapp.models.BestSellerBook
+import com.bumptech.glide.Glide
+
+
+
+
+
 
 /**
  * [RecyclerView.Adapter] that can display a [BestSellerBook] and makes a call to the
@@ -30,6 +40,14 @@ class BestSellerBooksRecyclerViewAdapter(
         holder.mItem = books[position]
         holder.mBookTitle.text = books[position].title
         holder.mBookAuthor.text = books[position].author
+        holder.mBookDesc.text = books[position].description
+        holder.mBookRank.text = String.format("%d", books[position].rank)
+        val bestSellerBook = books[position]
+        Glide.with(holder.mView)
+            .load(bestSellerBook.bookImageUrl)
+            .centerInside()
+            .into(holder.mBookImage)
+
         holder.mView.setOnClickListener {
             holder.mItem?.let { book ->
                 mListener?.onItemClick(book)
@@ -44,6 +62,9 @@ class BestSellerBooksRecyclerViewAdapter(
     inner class BookViewHolder(val mView: View) : ViewHolder(mView) {
         val mBookTitle: TextView = mView.findViewById<View>(id.book_title) as TextView
         val mBookAuthor: TextView = mView.findViewById<View>(id.book_author) as TextView
+        val mBookDesc: TextView = mView.findViewById<View>(id.book_description) as TextView
+        val mBookImage: ImageView = mView.findViewById<View>(id.book_image) as ImageView
+        val mBookRank: TextView = mView.findViewById<View>(id.ranking) as TextView
         var mItem: BestSellerBook? = null
 
         override fun toString(): String {
